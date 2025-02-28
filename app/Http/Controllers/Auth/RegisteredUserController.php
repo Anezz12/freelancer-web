a7878260d6ec6b2122d841e45073e914ebcf5a95
Harsena Argretya <108923452+Anezz12@users.noreply.github.com>
2025-02-28 21:48:09 +0700

    Add user registration enhancements: include avatar upload, occupation field, and role selection; initialize user wallet

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Wallet;
+use App\Models\WalletTransaction;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -31,15 +33,32 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'avatar' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg, webp'],
+            'occupation' => ['required', 'string', 'max:255'],
+            'role' => ['required', 'string', 'max:255', 'in:project_client,project_freelancer'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        if ($request->hasFile('avatar')) {
+            $avatarPath = $request->file('avatar')->store('images', 'public');
+        }
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'occupation' => $request->occupation,
+            'avatar' => $avatarPath,
+            'connect' => 10,
             'password' => Hash::make($request->password),
         ]);
+
+        $user->assignRole($request->role);
+
+        $wallet = new Wallet([
+            'balance' => 0,
+        ]);
+        $user->wallet()->save($wallet);
 
         event(new Registered($user));
 
